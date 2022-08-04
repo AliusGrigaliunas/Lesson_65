b2adca01b90d144fcfa1e692b0cb3a4f3684ec18
AliusGrigaliunas <Alius.grigaliunas@gmail.com>
2022-08-04 12:13:44 +0300

    feat(66):Finished 8/9 tasks and added a whole group of functions

--- src/TS-IndividualTask.ts
+++ src/TS-IndividualTask.ts
@@ -39,7 +39,7 @@
     * Užduočių rezultatų pateikimas tike pat svarbus, kiek sprendimas.
 */
 
-// 10 min
+// 10 min - 8:45(finished them at 8:30);
 console.groupCollapsed('1. Sukurkite funkciją, kuri atspausdiną tekstą didžiosiomis raidėmis');
 {
   const ToUpperCase = (text: string): string => text.toUpperCase();
@@ -48,7 +48,7 @@ console.groupCollapsed('1. Sukurkite funkciją, kuri atspausdiną tekstą didži
 }
 console.groupEnd();
 
-// 10 min
+// 10 min - 8:55(finished them at 8:35)
 console.groupCollapsed('2. Sukurkite funkciją, kuri grąžina pirmo ir antro parametro bendrą simbolių skaičių');
 {
   const CombinedSimbolsNum = (text: string, text2: string): number => text.split('').length + text2.split('').length;
@@ -57,48 +57,72 @@ console.groupCollapsed('2. Sukurkite funkciją, kuri grąžina pirmo ir antro pa
 }
 console.groupEnd();
 
-// 10 min
+// 10 min - 9:15(finished them at 8:57)
 console.groupCollapsed('3. Sukurkite funkciją, kuri grąžina <true>, jeigu žodyje yra 2 parametru perduoda raidė, priešingu atveju false');
 {
-
+  const CheckStrForLett = (word: string, letter: string) => word.includes(letter);
+  console.log(CheckStrForLett('Labas', 'e'));
 }
 console.groupEnd();
 
-// 10 min
+// 10 min (finished at 9:12);
 console.groupCollapsed('4. Sukurkite funkciją, kuri grąžina <true>, jeigu žodyje yra lyginis skaičius simbolių');
 {
+  const CheckStrSimbEqualNum = (str: string): boolean => { if (str.split('').length % 2 !== 0) { return false; } return true; };
 
+  console.log(CheckStrSimbEqualNum('Labap'));
 }
 console.groupEnd();
 
-// 15 min
+// 15 min (finished at 9:24)
 console.groupCollapsed('5. Sukurkite funkciją, kuri grąžina balsių kiekį žodyje');
 {
+  const checkStrIfVowels = (str: string) => {
+    const HasVowels = str.match(/[AaĄaEeĘęĖėIiĮįYyOoUuŲųŪū]/ig);
+    return HasVowels?.length;
+  };
 
+  console.log(checkStrIfVowels('Labas'));
 }
 console.groupEnd();
 
-// 15 min
+// 15 min (finished at 9:35)
 console.groupCollapsed('6. Sukurkite funkciją, kuri grąžina bet kokios raidės kiekį žodyje');
 {
+  const CheckSimbolInStrNum = (word: string, lett: string) => {
+    const lettCount = word.match(new RegExp(lett, 'g'))?.length;
+    return lettCount;
+  };
 
+  console.log(CheckSimbolInStrNum('Fool', 'o'));
 }
 console.groupEnd();
 
-// 15 min
+// 15 min (finished at 9:55)
 console.groupCollapsed('7. Sukurkite funkciją, kuri ištrintų pirmą surastą simbolį žodyje ir grąžintų pakeistą žodį');
 {
+  const RemoveSimbolFromStr = (str: string, lett: string) => {
+    const replacedLettStr = str.replace(str.split('')[0], lett);
+    return replacedLettStr;
+  };
 
+  console.log(RemoveSimbolFromStr('Nick', 'R'));
 }
 console.groupEnd();
 
-// 20 min
+// 20 min (finished at 11:20)
 //  7. Sukurkite funkciją, kuri pirmu parametru priimtų žodį, o antruoju - masyvą su raidėmis.
 //  Ši funkcija turi žodyje ištrinti visas raides, kurios perduotos antruoju parametru.
 //  Atlikus veiksmus, grąžinti pakeistą žodį
-console.groupCollapsed('7. Sukurkite funkciją, kuri pirmu parametru priimtų žodį, o antruoju - masyvą su raidėmis.');
+console.groupCollapsed('8. Sukurkite funkciją, kuri pirmu parametru priimtų žodį, o antruoju - masyvą su raidėmis.');
 {
+  const ReplaceStrWithStr = (str1: string, strLett: string): string => {
+    const StrArr = strLett.split('');
+    return StrArr.reduce((prevLett, currLett) => prevLett.replaceAll(currLett, ''), str1);
+  };
 
+  const newWord = ReplaceStrWithStr('Labass', 'ass');
+  console.log(newWord);
 }
 console.groupEnd();
 
